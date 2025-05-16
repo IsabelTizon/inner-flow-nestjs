57a1e0b171b504666902d66b01418e8c085dbf33
Isabel <tizonarranz@gmail.com>
2025-05-16 22:44:44 +0200

    validation with class-validator in add-pose-dto

--- src/poses/dtos/add-pose.dto.ts
+++ src/poses/dtos/add-pose.dto.ts
@@ -1,5 +1,11 @@
+import { IsString } from 'class-validator';
 export class addPoseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString()
   readonly image: string;
 }
