e5c5948a1536ffd8bd163d79a348b62825db3c10
Isabel <tizonarranz@gmail.com>
2025-05-17 08:15:24 +0200

    validation decorator updated on add-pose-dto
    https://github.com/typestack/class-validator?tab=readme-ov-file#validation-decorators

--- src/poses/dtos/add-pose.dto.ts
+++ src/poses/dtos/add-pose.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 export class addPoseDto {
   @IsString()
+  @Length(3, 30)
   readonly name: string;
 
   @IsString()
