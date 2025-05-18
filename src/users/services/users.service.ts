d5d940db41d8a53dc70d8ebbf790e1c4c7324aa5
Isabel <tizonarranz@gmail.com>
2025-05-18 11:40:55 +0200

    sequences entity created

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Poses } from 'src/poses/models/poses.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private posesDDBB: Poses[] = [
+    // poses DDBB simulator
+    {
+      id: uuidv4(),
+      name: 'Perro boca abajo',
+      image: 'downward-dog-abajo.jpg',
+      description:
+        'Estira la espalda y fortalece los brazos para hacer el perro boca abajo.',
+    },
+    {
+      id: uuidv4(),
+      name: 'Perro boca arriba',
+      image: 'downward-dog-arriba.jpg',
+      description:
+        'Estira la espalda y fortalece los brazos para hacer el perro boca arriba.',
+    },
+    {
+      id: uuidv4(),
+      name: 'Perro boca medio',
+      image: 'downward-dog-medio.jpg',
+      description:
+        'Estira la espalda y fortalece los brazos para hacer el perro boca medio.',
+    },
+  ];
+}
