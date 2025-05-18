bd3e9f7bad1e56b82599965ca47290b5380fefe0
Isabel <tizonarranz@gmail.com>
2025-05-18 10:32:00 +0200

    users folder (controller, services and module) updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PosesModule } from './poses/poses.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PosesModule],
+  imports: [PosesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
