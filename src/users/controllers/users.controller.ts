658ffbfbd98318b1db2d61860f79064a653a2482
Isabel <tizonarranz@gmail.com>
2025-05-19 08:16:58 +0200

    users commented

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -3,18 +3,29 @@ import { UsersService } from '../services/users.service';
 import { CreateUserDto } from '../dtos/createUser.dto';
 import { User } from '../models/user.model';
 
+// url path: /users
 @Controller('users')
 export class UsersController {
+  //Injects the UsersService service to apply dependency injection.
   constructor(private readonly usersService: UsersService) {}
 
+  // url path: //users/register
   @Post('register')
+  //@Body() extracts the body of the HTTP request (e.g., registration data)
+  //createUserDto is the object containing the validated data for the new user.
+  // @Body() extracts the body of the HTTP request (e.g., registration data).
   createUser(@Body() createUserDto: CreateUserDto): User {
+    //Calls the usersService service to create the user and returns the result.
     return this.usersService.createUser(createUserDto);
   }
 
+  // url path: /users/123/sequences
   @Get(':id/sequences')
   //@Param('id') userId: Take the URL parameter called id and store it in the userId variable
+  // @Param('id'): Extracts the ID from the URL.
+  // userId: string: Typed as a string.
   getUserSequences(@Param('id') userId: string) {
+    //Call the service to get the user's strings with that ID and return them.
     return this.usersService.getUserSequences(userId);
   }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -5,11 +5,15 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
+  // A private array of users is created in memory, like a small simulated database
   private users: User[] = [];
 
+  //Create a new user. Receive data from the DTO.
   createUser(createUserDto: CreateUserDto): User {
+    //Extracts (deconstructs) the DTO properties: name, email, and password.
     const { name, email, password } = createUserDto;
 
+    //Create a new User object
     const newUser: User = {
       id: uuidv4(),
       name,
@@ -18,6 +22,7 @@ export class UsersService {
       sequences: [],
     };
 
+    // Save the new user to the users array
     this.users.push(newUser);
     console.log('New user registered:', newUser);
     return newUser;
