13c244ce4d8ad3957d0cf7a6fa4d13b47dbf0365
Isabel <tizonarranz@gmail.com>
2025-05-20 12:32:05 +0200

    Sequence folder deleted

--- src/sequences/controllers/sequences.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SequencesService } from '../services/sequences.service';
-
-@Controller('sequences')
-export class SequencesController {
-  constructor(private readonly sequences: SequencesService) {}
-}
