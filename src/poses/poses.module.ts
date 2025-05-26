f2e4f368d169417f8f5ff0ebf510a5d3efbb953e
Isabel <tizonarranz@gmail.com>
2025-05-26 20:42:36 +0200

    AIService created

--- src/poses/poses.module.ts
+++ src/poses/poses.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PosesService } from './services/poses.service';
 import { PosesController } from './controllers/poses.controller';
 import { DescriptionService } from './services/description.service';
+import { AIService } from './services/ai.service';
 
 @Module({
   controllers: [PosesController],
-  providers: [PosesService, DescriptionService],
+  providers: [PosesService, DescriptionService, AIService],
   exports: [PosesService],
 })
 export class PosesModule {}
--- src/poses/services/description.service.ts
+++ src/poses/services/description.service.ts
@@ -1,32 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { createPoseDto } from '../dtos/pose.dto';
-import axios from 'axios';
+import { AIService } from './ai.service';
 
 @Injectable()
 export class DescriptionService {
+  constructor(private readonly aiService: AIService) {}
+
   async createDescription(poseDto: createPoseDto): Promise<string> {
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      {
-        model: 'gpt-4-turbo',
-        messages: [
-          {
-            role: 'user',
-            content: `Create a pose's description of ${poseDto.name}`,
-          },
-        ],
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`,
-        },
-      },
+    const response = await this.aiService.ask(
+      `Create a description of ${poseDto.name}`,
     );
 
-    console.log(response.data);
-    return response.data;
-    // return response.data.choices[0].message.content;
+    return response;
   }
 }
 //
