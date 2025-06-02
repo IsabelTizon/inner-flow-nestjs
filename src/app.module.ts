39197c7bbe48852326ac68d58119998f10c5bd36
Isabel <tizonarranz@gmail.com>
2025-06-02 17:26:25 +0200

    TypeOrmModule imported into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PosesModule } from './poses/poses.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PosesModule, UsersModule],
+  imports: [
+    PosesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'root',
+      database: 'yogaDDBB.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
