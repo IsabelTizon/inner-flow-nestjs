de08f398cdc80c6c4970006503796dc708e858e4
Isabel <tizonarranz@gmail.com>
2025-06-02 17:44:03 +0200

    typeorm decorators added on poses.model

--- src/poses/models/poses.model.ts
+++ src/poses/models/poses.model.ts
@@ -1,6 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('poses')
 export class Poses {
+  @PrimaryGeneratedColumn({})
   public readonly id: string;
+  @Column()
   public name: string;
+
+  @Column()
   public description: string;
+
+  @Column()
   public image: string;
 }
