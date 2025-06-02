5cb24f87727fa18d920c44c3c9597db323d38803
Isabel <tizonarranz@gmail.com>
2025-06-02 19:41:10 +0200

    TypeOrmModule.forFeature([Poses]) imported into users.module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { PosesModule } from '../poses/poses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Poses } from '../poses/models/poses.model'; // Adjust the import path as necessary
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [PosesModule],
+  imports: [PosesModule, TypeOrmModule.forFeature([Poses])], // Add your entities here
 })
 export class UsersModule {}
