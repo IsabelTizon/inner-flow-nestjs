8b7cd79c10cb429c57c70dfb67426f4ecd0b6b87
Isabel <tizonarranz@gmail.com>
2025-06-04 16:14:48 +0200

    error commented

--- src/poses/models/poses.model.ts
+++ src/poses/models/poses.model.ts
@@ -2,6 +2,10 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('poses')
 export class Poses {
+  // Error:QueryFailedError: SQLITE_MISMATCH: datatype mismatch
+  // Solution @PrimaryGeneratedColumn('uuid')
+  // uuid was added in @PrimaryGeneratedColumn() porque sin especificar un tipo, TypeORM estaba usando un entero autoincremental por defecto, pero la tabla estaba definida con el tipo TEXT para la columna id.
+
   @PrimaryGeneratedColumn('uuid')
   public readonly id: string;
   @Column()
