c337c798a47cb3d21278fdca311c186a9d8301d0
Isabel <tizonarranz@gmail.com>
2025-06-04 15:41:46 +0200

    getAll, getOne, addPose, delete and update change with TypeOrm

--- src/poses/poses.module.ts
+++ src/poses/poses.module.ts
@@ -3,10 +3,13 @@ import { PosesService } from './services/poses.service';
 import { PosesController } from './controllers/poses.controller';
 import { DescriptionService } from './services/description.service';
 import { AIService } from './services/ai.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Poses } from './models/poses.model';
 
 @Module({
   controllers: [PosesController],
   providers: [PosesService, DescriptionService, AIService],
   exports: [PosesService],
+  imports: [TypeOrmModule.forFeature([Poses])],
 })
 export class PosesModule {}
--- src/poses/services/poses.service.ts
+++ src/poses/services/poses.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
 import { NotFoundException } from '@nestjs/common';
 import { createPoseDto, updatePoseDto } from '../dtos/pose.dto';
 import { DescriptionService } from './description.service';
@@ -7,6 +6,7 @@ import * as sqlite3 from 'sqlite3';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Poses } from '../models/poses.model';
+import { BadRequestException } from '@nestjs/common';
 
 // export interface Poses {
 //   id: string;
@@ -48,49 +48,52 @@ export class PosesService {
     });
   }
 
-  private posesDDBB: Poses[] = [];
+  // private posesDDBB: Poses[] = [];
   private database: sqlite3.Database;
   private descriptionService: DescriptionService;
 
   // GET ALL THE POSES
   async getAll(): Promise<Poses[]> {
-    return new Promise((resolve, reject) => {
-      this.database.all(`SELECT * FROM poses`, [], (err, rows) => {
-        if (err) {
-          console.error('Error retrieving all poses:', err);
-          return reject(err);
-        }
-        resolve(rows as Poses[]);
-      });
-    });
+    return this.posesRepository.find();
+    // return new Promise((resolve, reject) => {
+    //   this.database.all(`SELECT * FROM poses`, [], (err, rows) => {
+    //     if (err) {
+    //       console.error('Error retrieving all poses:', err);
+    //       return reject(err);
+    //     }
+    //     resolve(rows as Poses[]);
+    //   });
+    // });
   }
 
   // GET ONE POSE BY ID
   async getOne(id: string): Promise<Poses> {
     console.log(`Getting pose with id: ${id}`);
 
-    return new Promise((resolve, reject) => {
-      this.database.get(
-        `SELECT * FROM poses WHERE id = ?`,
-        [id],
-        (error, row) => {
-          if (error) {
-            console.error('Error fetching pose from database:', error);
-            return reject(
-              new NotFoundException(`Pose with id ${id} not found`),
-            );
-          }
+    return this.posesRepository.findOneOrFail({ where: { id: id } });
 
-          if (!row) {
-            return reject(
-              new NotFoundException(`Pose with id ${id} not found`),
-            );
-          }
+    // return new Promise((resolve, reject) => {
+    //   this.database.get(
+    //     `SELECT * FROM poses WHERE id = ?`,
+    //     [id],
+    //     (error, row) => {
+    //       if (error) {
+    //         console.error('Error fetching pose from database:', error);
+    //         return reject(
+    //           new NotFoundException(`Pose with id ${id} not found`),
+    //         );
+    //       }
 
-          resolve(row as Poses);
-        },
-      );
-    });
+    //       if (!row) {
+    //         return reject(
+    //           new NotFoundException(`Pose with id ${id} not found`),
+    //         );
+    //       }
+
+    //       resolve(row as Poses);
+    //     },
+    //   );
+    // });
   }
 
   // CREATE A NEW POSE
@@ -103,66 +106,99 @@ export class PosesService {
         ? poseDto.description
         : await this.description.createDescription(poseDto);
 
-    const newPose: Poses = {
-      id: uuidv4(),
-      name: poseDto.name,
-      image: poseDto.image,
+    const newPose = this.posesRepository.create({
+      ...poseDto,
       description,
-    };
-
-    return new Promise((resolve, reject) => {
-      this.database.run(
-        `INSERT INTO poses (id, name, description, image) VALUES (?, ?, ?, ?)`,
-        [newPose.id, newPose.name, newPose.description, newPose.image],
-        (err) => {
-          if (err) {
-            console.error('Error inserting pose:', err);
-            return reject(err);
-          }
-          resolve(newPose);
-        },
-      );
     });
+
+    await this.posesRepository.save(newPose);
+    return newPose;
+    // const newPose: Poses = {
+    //   id: uuidv4(),
+    //   name: poseDto.name,
+    //   image: poseDto.image,
+    //   description,
+    // };
+
+    // return new Promise((resolve, reject) => {
+    //   this.database.run(
+    //     `INSERT INTO poses (id, name, description, image) VALUES (?, ?, ?, ?)`,
+    //     [newPose.id, newPose.name, newPose.description, newPose.image],
+    //     (err) => {
+    //       if (err) {
+    //         console.error('Error inserting pose:', err);
+    //         return reject(err);
+    //       }
+    //       resolve(newPose);
+    //     },
+    //   );
+    // });
   }
 
   //DELETE A POSE BY ID
   async delete(id: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.database.run(`DELETE FROM poses WHERE id = ?`, [id], function (err) {
-        if (err) {
-          return reject(
-            new NotFoundException(`Error deleting pose with id ${id}`),
-          );
-        }
-        if (this.changes === 0) {
-          return reject(new NotFoundException(`Pose with id ${id} not found`));
-        }
-        resolve();
-      });
-    });
+    await this.posesRepository.delete(id);
+    // return new Promise((resolve, reject) => {
+    //   this.database.run(`DELETE FROM poses WHERE id = ?`, [id], function (err) {
+    //     if (err) {
+    //       return reject(
+    //         new NotFoundException(`Error deleting pose with id ${id}`),
+    //       );
+    //     }
+    //     if (this.changes === 0) {
+    //       return reject(new NotFoundException(`Pose with id ${id} not found`));
+    //     }
+    //     resolve();
+    //   });
+    // });
   }
 
   // UPDATE A POSE BY ID
   async update(id: string, poseDto: updatePoseDto): Promise<void> {
-    const { name, description, image } = poseDto;
+    const existingPose = await this.posesRepository.findOne({ where: { id } });
 
-    return new Promise((resolve, reject) => {
-      this.database.run(
-        `UPDATE poses SET name = ?, description = ?, image = ? WHERE id = ?`,
-        [name, description, image, id],
-        function (err) {
-          if (err) {
-            return reject(err);
-          }
-          if (this.changes === 0) {
-            return reject(
-              new NotFoundException(`Pose with id ${id} not found`),
-            );
-          }
-          resolve();
-        },
+    if (!existingPose) {
+      throw new NotFoundException(`Pose with id ${id} not found`);
+    }
+
+    if (!poseDto.name || !poseDto.image) {
+      throw new BadRequestException(
+        'Name and image are required to generate a description',
       );
+    }
+
+    const updatedDescription = poseDto.description?.trim()
+      ? poseDto.description
+      : await this.description.createDescription({
+          name: poseDto.name,
+          image: poseDto.image,
+          description: '',
+        });
+
+    await this.posesRepository.update(id, {
+      name: poseDto.name ?? existingPose.name,
+      description: updatedDescription,
+      image: poseDto.image ?? existingPose.image,
     });
+
+    // const { name, description, image } = poseDto;
+    // return new Promise((resolve, reject) => {
+    //   this.database.run(
+    //     `UPDATE poses SET name = ?, description = ?, image = ? WHERE id = ?`,
+    //     [name, description, image, id],
+    //     function (err) {
+    //       if (err) {
+    //         return reject(err);
+    //       }
+    //       if (this.changes === 0) {
+    //         return reject(
+    //           new NotFoundException(`Pose with id ${id} not found`),
+    //         );
+    //       }
+    //       resolve();
+    //     },
+    //   );
+    // });
   }
 
   // SEARCH POSES BY NAME
