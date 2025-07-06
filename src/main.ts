609d7236963df24fd33146c171de3d8ff182fd0b
Isabel <tizonarranz@gmail.com>
2025-07-06 10:59:32 +0200

    CORS added to allows frontend to request resources from different domain securely

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS: CORS added to allows frontend to request resources from different domain securely
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    // origin: 'https://inner-flow.com',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
