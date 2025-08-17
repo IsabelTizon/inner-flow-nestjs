3e602059e22dc6bf13ffbbb7acde7e72c8d7f0cd
Isabel <tizonarranz@gmail.com>
2025-08-17 17:39:39 +0200

    Add ContactModule with controller and email service
    Introduces ContactModule in the contact feature, registering ContactController and EmailService, and exporting EmailService for use in other modules.

---
+++ src/contact/contact.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ContactController } from './contact.controller';
+import { EmailService } from './services/email.service';
+
+@Module({
+  controllers: [ContactController],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class ContactModule {}
+
+
